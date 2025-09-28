a4b90b76eccf2c7339bd9c3ae2ec9fece737dc22
AntiHero2311 <hungntse184131@fpt.edu.vn>
2025-09-28 20:41:01 +0700

    fix ui map 2

--- Assets/_Game/Core/UIMainMenu.cs
+++ Assets/_Game/Core/UIMainMenu.cs
@@ -4,20 +4,86 @@ using UnityEngine.UI;
 public class UIMainMenu : MonoBehaviour
 {
     private ISceneLoader sceneLoader => SceneLoader.Instance;
+
     [SerializeField]
     private Button start;
     [SerializeField]
     private Button exit;
-    [SerializeField] 
+    [SerializeField]
     private Button settings;
     [SerializeField]
     private Button deckButton;
+
     private void OnEnable()
     {
-        start.onClick.RemoveAllListeners();
-        start.onClick.AddListener(() => sceneLoader.LoadSceneName("Map").Execute());
-        exit.onClick.RemoveAllListeners();
-        exit.onClick.AddListener(() => Application.Quit());
-        deckButton.GetComponent<Image>().sprite = GameInstance.Singleton.GetDeckData().DeckCover;
+        // Kiểm tra null cho các button
+        if (start != null)
+        {
+            start.onClick.RemoveAllListeners();
+
+            // Kiểm tra sceneLoader trước khi sử dụng
+            if (sceneLoader != null)
+            {
+                start.onClick.AddListener(() => sceneLoader.LoadSceneName("Map").Execute());
+            }
+            else
+            {
+                Debug.LogError("SceneLoader.Instance is null!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Start button is not assigned!");
+        }
+
+        if (exit != null)
+        {
+            exit.onClick.RemoveAllListeners();
+            exit.onClick.AddListener(() => Application.Quit());
+        }
+        else
+        {
+            Debug.LogError("Exit button is not assigned!");
+        }
+
+        // Kiểm tra deckButton và các dependencies
+        if (deckButton != null)
+        {
+            var deckImage = deckButton.GetComponent<Image>();
+            if (deckImage != null)
+            {
+                // Kiểm tra GameInstance và DeckData
+                if (GameInstance.Singleton != null)
+                {
+                    var deckData = GameInstance.Singleton.GetDeckData();
+                    if (deckData != null && deckData.DeckCover != null)
+                    {
+                        deckImage.sprite = deckData.DeckCover;
+                    }
+                    else
+                    {
+                        Debug.LogError("DeckData or DeckCover is null!");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("GameInstance.Singleton is null!");
+                }
+            }
+            else
+            {
+                Debug.LogError("DeckButton doesn't have Image component!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Deck button is not assigned!");
+        }
+    }
+
+    // Alternative: Sử dụng Start() thay vì OnEnable() nếu cần đảm bảo các Singleton đã được khởi tạo
+    private void Start()
+    {
+        // Di chuyển logic từ OnEnable() xuống đây nếu cần
     }
 }
